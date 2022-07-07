aca5dc9d91268ef4815968c247e6b739be806da6
Rabah <aminurabah@yahoo.com>
2022-07-07 07:11:29 -0700

    0 task. c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,14 @@
-#include " main.h"
+#include "main.h"
 
 /**
-* _isupper -> this is a function to check upper case alphas
-*@c: an integer paramer
-* Returb: something but success
+* _isuper -> this is afunction to check upper case alphas
+* @c: an integer paramer
+* Return : something but success
 */
 int _isupper(int c)
 {
-return (c >= 'A' && c <= 'Z');
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+	return (c >= 65 && c <= 90);
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c:  The character to print
+ * _putchar - write the character c to stdout
+ * @c: The character to print
  *
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropeiately.
+ * Return: On success 1.
+ * On error, -1 is  returned, and errno is set approprietly.
  */
-int _puchar(char c)
+int _putchar(char c)
 {
-return (write(1, & c, 1));
+	return (writ(1, &cm 1));
 }
