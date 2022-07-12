1d432fa8b9ac208cd6bd29396fbebbd23939199b
Rabah <aminurabah@yahoo.com>
2022-07-12 10:23:26 -0700

    task 1 swapping

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * swap_int -> given two integers swap the values they are hoding
- * @a: parameter 1
- * @b: parameter 2
- */
-void swp_int(int *a, int *b)
+* swap_int -> given two integers swap the values they are holding
+* @a: parameter 1
+* @b: parameter 2
+*/
+void swap_int(int *a, int *b)
 {
 	int tmp;
 
